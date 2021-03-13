3fb7fb9e85b34e952f62b95e988eaec0e9a84949
Michael Sun <michael.sun@bindo.com>
2021-03-13 22:58:34 +0800

    1. 修改  ` ` 代码块样式和提示信息的 Material Design 的配色\n2. 格式化了一些代码

--- assets/js/joe.archive.js
+++ assets/js/joe.archive.js
@@ -1,16 +1,24 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Michael Sun @ www.cctv3.net
+ * @Date: 2021-03-12 22:11:52
+ * @LastEditors: Michael Sun
+ * @LastEditTime: 2021-03-13 21:56:18
+ */
 /* 搜索页面需要用到的JS */
-document.addEventListener('DOMContentLoaded', function () {
-    /* 激活列表特效 */
-    {
-        var wow = $('.joe_archive__list').attr('data-wow');
-        if (wow !== 'off' && wow)
-            new WOW({
-                boxClass: 'wow',
-                animateClass: 'animated '.concat(wow),
-                offset: 0,
-                mobile: true,
-                live: true,
-                scrollContainer: null
-            }).init();
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  /* 激活列表特效 */
+  {
+    var wow = $(".joe_archive__list").attr("data-wow");
+    if (wow !== "off" && wow)
+      new WOW({
+        boxClass: "wow",
+        animateClass: "animated ".concat(wow),
+        offset: 0,
+        mobile: true,
+        live: true,
+        scrollContainer: null,
+      }).init();
+  }
 });
